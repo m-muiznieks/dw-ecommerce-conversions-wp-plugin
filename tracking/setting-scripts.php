4190b05043ce02fd795ca630ccd7c3baba6948a2
m-muiznieks <martins.muiznieks@digiezi.lv>
2024-10-16 03:31:28 +0300

    modified:   tracking/setting-scripts.php

--- tracking/setting-scripts.php
+++ tracking/setting-scripts.php
@@ -6,37 +6,35 @@ include_once(DW_BASE_DIR . 'tracking/datalayer-events/purchase-event.php');
 
 
 //INSERT GOOGLE TAG MANAGER
-function dw_insert_gtm() {
-    $gtm_id = esc_js(get_option('digiezi_gtm_id'));
-    if (!$gtm_id) return;
-
-    // Header script
-    add_action('wp_head', function() use ($gtm_id) {
-        ?>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer', '<?php echo $gtm_id; ?>');</script>
-        <!-- End Google Tag Manager -->
-        <?php
-    }, 1);
-
-    // Body script
-    add_action('wp_footer', function() use ($gtm_id) {
-        ?>
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo esc_attr($gtm_id); ?>"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
-        <?php
-    }, 1);
+function dw_activate_gtm_script() {
+  add_action('wp_head', 'dw_insert_gtm_header', 1);
+  add_action('wp_body_open', 'dw_insert_gtm_body', 1);
 }
 
-// Replace the existing GTM activation with this:
-if(get_option('digiezi_gtm_id')) { dw_insert_gtm(); }
-    
+// Function to insert GTM script in the <head>
+function dw_insert_gtm_header() {
+  $gtm_id = esc_js(get_option('digiezi_gtm_id'));
+  ?>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer', '<?php echo $gtm_id; ?>');</script>
+  <!-- End Google Tag Manager -->
+  <?php
+}
+
+// Function to insert GTM noscript after the <body> tag
+function dw_insert_gtm_body() {
+  $gtm_id = esc_attr(get_option('digiezi_gtm_id'));
+  ?>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo $gtm_id; ?>"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <?php
+}
+
 //INSERT HEADER SCRIPTS
 function dw_activate_header_script(){
   add_action('wp_head', 'insert_header', 1);
@@ -62,7 +60,7 @@ function dw_activate_footer_script() {
 }
 
 //GOOGLE TAG MANAGER
-if(get_option('digiezi_gtm_id')) { dw_insert_gtm(); }
+if(get_option('digiezi_gtm_id')) { dw_activate_gtm_script(); }
     
 //HEADER SCRIPT
 if(get_option('digiezi_header_scripts')) { dw_activate_header_script(); }
